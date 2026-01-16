ebbea2e54b8d2a8c5f5f83914f85991de44d22fd
Riancahyo <riancahyo75@gmail.com>
2026-01-16 07:44:40 +0700

    Pagination

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,5 @@
 import Product from "../models/Product.js";
+import { Op } from "sequelize";
 
 export const createProduct = async (req, res) => {
   try {
@@ -11,8 +12,35 @@ export const createProduct = async (req, res) => {
 
 export const getProducts = async (req, res) => {
   try {
-    const products = await Product.findAll();
-    res.json(products);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const search = req.query.search || "";
+    const sort = req.query.sort || "createdAt"; 
+    const order = req.query.order || "DESC"; 
+
+    const offset = (page - 1) * limit;
+
+    const where = search
+      ? {
+          name: {
+            [Op.like]: `%${search}%`,
+          },
+        }
+      : {};
+
+    const products = await Product.findAndCountAll({
+      where,
+      limit,
+      offset,
+      order: [[sort, order]],
+    });
+
+    res.json({
+      total: products.count,
+      current_page: page,
+      total_pages: Math.ceil(products.count / limit),
+      data: products.rows,
+    });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
