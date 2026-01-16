a976c28feae77b3e993a7c95ed5dc44896474641
Riancahyo <riancahyo75@gmail.com>
2026-01-16 07:58:19 +0700

    Error Handler

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import { swaggerDocs } from "./swagger.js";
 import rateLimit from "express-rate-limit";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 swaggerDocs(app);
 
@@ -17,6 +18,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use(errorHandler);
 
 sequelize.sync().then(() => {
   console.log("Database synced!");
